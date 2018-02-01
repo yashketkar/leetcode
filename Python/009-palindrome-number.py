603c09ca6d42cae030ae6ca0f2ba89a517c3cfc7
Yash Ketkar <yashketkar@outlook.com>
2018-01-31 22:24:03 -0600

    add solution for problem 009

---
+++ Python/009-palindrome-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        if x < 0 or (x % 10 == 0 and x != 0):
+            return False
+        revertedNumber = 0
+        while (x > revertedNumber):
+            revertedNumber = revertedNumber * 10 + x % 10
+            x /= 10
+        return x == revertedNumber or x == revertedNumber/10
